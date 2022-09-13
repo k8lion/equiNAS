858ebcd8c3cbbb0052e20615b3d532bfa16d9a92
Kaitlin Maile <kaitlinmaile@gmail.com>
2022-09-13 16:51:36 +0200

    pareto

--- utilities.py
+++ utilities.py
@@ -295,3 +295,16 @@ def getmodelsize(model, includebuffer=False, counts=True):
             buffer_size += buffer.nelement() * multiplier
         return buffer_size + param_size
     return param_size
+
+def is_pareto_efficient(costs):
+    """
+    Find the pareto-efficient points
+    :param costs: An (n_points, n_costs) array
+    :return: A (n_points, ) boolean array, indicating whether each point is Pareto efficient
+    """
+    is_efficient = np.ones(costs.shape[0], dtype = bool)
+    for i, c in enumerate(costs):
+        if is_efficient[i]:
+            is_efficient[is_efficient] = np.any(costs[is_efficient]<c, axis=1) 
+            is_efficient[i] = True 
+    return is_efficient
